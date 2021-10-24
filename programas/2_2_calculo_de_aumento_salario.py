2a766f65155f486a414538a5962870bb782c4daf
Luis Felipe Assmann <59901281+luisdef@users.noreply.github.com>
2021-10-24 16:08:33 -0300

    blank lines

--- programas/2_2_calculo_de_aumento_salario.py
+++ programas/2_2_calculo_de_aumento_salario.py
@@ -3,14 +3,11 @@
 
 # Declarar a variável salario
 salario = 1500
-
 # Declarar a variável aumento
 aumento = 5
-
 # Mostrar salário com o aumento aplicado,
 # com uma regra de três simples
 print(f"\nSalário = R$ {salario}\n"
       f"Aumento = {aumento} %\n"
       f"Salário com o aumento aplicado = R$ {salario + (salario * aumento / 100)}")
-
 input("\nDigite Enter para sair... ")
--- programas/2_3_aumento_de_salario_simples.py
+++ programas/2_3_aumento_de_salario_simples.py
@@ -1,6 +1,4 @@
 # Programa 2.3 P.48
 # Alternativa para o cálculo de aumento de salário
-
 print(f"Salário com aumento R$ {1500 + (1500 * 5 / 100)}")
-
 input("\nDigite Enter para sair... ")
