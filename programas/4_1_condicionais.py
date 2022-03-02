f824c06d425659cde623b789c992df50724aaa6f
luisdef <luis.assmann.1234@gmail.com>
2022-03-02 10:08:19 -0300

    programs

---
+++ programas/4_1_condicionais.py
@@ -0,0 +1,11 @@
+# Programa 4.1
+# Lê dois valores e imprime qual é o maior.
+
+print('Qual é o maior número?\n')
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+
+if a > b:
+    print(f'O primeiro valor {a} é maior que o segundo valor {b}.')
+if b > a:
+    print(f'O segundo valor {b} é maior que o primeiro valor {a}.')
