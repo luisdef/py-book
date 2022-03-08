a7df4dc78d13a498218fbd69bb508502429b2842
luisdef <luis.assmann.1234@gmail.com>
2022-03-07 22:47:41 -0300

    exercicios py

---
+++ exercicios/4_9_aprovacao_emprestimo.py
@@ -0,0 +1,11 @@
+# Exercício 4.9
+# Escreva um programa para aprovar o empréstimo 
+# bancário para compra de uma casa. O programa
+# deve perguntar o valor da casa a comprar, o
+# salário e a quantidade de anos a pagar. O valor 
+# da prestação mensal não pode ser superior a 30% do
+# salário. Calcule o valor da prestação como sendo o
+# valor da casa a comprar dividindo pelo número de 
+# meses a pagar.
+
+# Valor da casa; Salário; Anos a pagar. 
