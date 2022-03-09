f9a0a40a658e9992da59a3d7196feaa3f86f2378
luisdef <luis.assmann.1234@gmail.com>
2022-03-08 23:31:00 -0300

    programas

--- programas/4_2_estado_do_carro.py
+++ programas/4_2_estado_do_carro.py
@@ -2,7 +2,9 @@
 # Carro novo ou velho, dependendo da idade
 
 print('O carro é novo ou velho?')
+
 idade = int(input('Digite a idade do carro: '))
+
 if idade <= 3:
     print('O carro é novo.')
 if idade > 3:
