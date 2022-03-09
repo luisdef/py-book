672a187fa2c6c100c8192145a69dbc486bad4dcd
luisdef <luis.assmann.1234@gmail.com>
2022-03-08 23:32:48 -0300

    exercicios

--- exercicios/4_8_calculo_operacoes.py
+++ exercicios/4_8_calculo_operacoes.py
@@ -1,12 +1,14 @@
 # Exercício 4.8
 # Escreva um programa que leia dos números e que pergunte
-# qual operação você deseja realizar. Você deve poder
+# qual operação deseja realizar. Você deve poder
 # calcular soma (+), subtração (-), multiplicação (*) e 
 # divisão (/). Exiba o resultado da operação solicitada.
 
 print('Calculadora simples de operações:\n')
+
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
+
 operacao = input('''Opções:
   (+) Adição;
   (-) Subtração;
