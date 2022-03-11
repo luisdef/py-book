d5c08c3a58ca4108a695f38d443dbf143a96c12c
luisdef <luis.assmann.1234@gmail.com>
2022-03-10 23:45:59 -0300

    exercicios 4.6

--- exercicios/4_6_ticket_passagem.py
+++ exercicios/4_6_ticket_passagem.py
@@ -4,11 +4,8 @@
 # Calcule o preço da passagem, cobrando R$ 0,50
 # por km para viagens de até 200 km, e R$ 0,45
 # para viagens mais longas.
-
 print('Cálculo do valor do ticket de uma passagem:\n')
-
 distancia = int(input('Distância que irá percorrer [km]: '))
-
 if distancia <= 200:
     valor_passagem = distancia * 0.50
     print(f'\nValor da passagem: R$ {valor_passagem:.2f}.')
